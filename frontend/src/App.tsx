5d28115ce8eba5b99551cdc15a1e8505be78a3d0
LinguiniThePasta <lfh20130915@gmail.com>
2023-12-01 19:06:15 -0500

    init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import SearchBar from "./components/Search Bar.tsx";
+
+function App() {
+
+  return (
+    <>
+      <SearchBar></SearchBar> hi
+    </>
+  )
+}
+
+export default App
