0d905f96e19fd7ba81ae0b956837ea0d84d40af6
LinguiniThePasta <lfh20130915@gmail.com>
2023-12-21 00:24:19 -0500

    Finished Responsive Design for the About me page. Finished all responsive Design.

--- client/src/components/Contact.tsx
+++ client/src/components/Contact.tsx
@@ -15,14 +15,13 @@ function Contact() {
     }
     return (
         <>
-            <div className={"pl-40 flex-col bg-gray-100"}>
-                <form onSubmit={handleSubmit}>
-                    <div className={"text-5xl font-bold py-10"}>Contact <button type={"button"}>Me!</button></div>
-
-                    <div className={"py-10"}>
+            <div className={"flex flex-col items-center md:items-start pl-10 md:pl-40 bg-gray-100 transition-all delay-75 duration-300"}>
+                <form onSubmit={handleSubmit} className={"flex flex-col items-center md:items-start"}>
+                    <div className={"text-center md:text-right text-5xl font-bold py-10 transition-all delay-75 duration-300"}>Contact <button type={"button"}>Me!</button></div>
+                    <div className={"py-10 transition-all delay-75 duration-300"}>
                         <input onChange={event => setEmail(event.target.value)} className={"w-96 text-gray-900 text-md rounded-lg p-3 transition-all delay-75 duration-300 ease-in-out ring-2 ring-gray-200 hover:outline-none hover:ring-2 hover:ring-black"} type={"email"} placeholder={"Email"}/>
                     </div>
-                    <div className={""}>
+                    <div className={"transition-all delay-75 duration-300"}>
                         <textarea onChange={event => setMessage(event.target.value)} className={"w-96 h-44 text-gray-900 text-md rounded-lg p-3 transition-all delay-75 duration-300 ease-in-out ring-2 ring-gray-200 hover:outline-none hover:ring-2 hover:ring-black"} placeholder={"Your Message Here"}/>
                     </div>
                     <button className={"rounded-lg px-6 py-4 font-inter text-lg my-10 transition-all delay-75 duration-300 ease-in-out ring-2 ring-gray-200 hover:outline-none hover:ring-2 hover:ring-black"}>Contact Me</button>
--- client/src/pages/About Me.tsx
+++ client/src/pages/About Me.tsx
@@ -6,23 +6,24 @@ function AboutMe() {
     return (
         <>
             <Navbar />
-            <div className={"flex flex-col items-center md:flex-row justify-between bg-gray-100 py-10"}>
-                <div className={"flex-col pl-40"}>
-                    <div className={"flex text-5xl font-bold py-12"}>Hi, I'm Lingyu</div>
-                    <div className={"w-2/3"}>
+            <div className={"flex flex-col align-middle md:items-center md:flex-row md:justify-between bg-gray-100 py-10"}>
+                <div className={"flex-col pl-28 md:pl-40 pb-10"}>
+                    <div className={"text-5xl w-2/3 text-center md:text-left font-bold py-12"}>Hi, I'm Lingyu</div>
+                    <div className={"w-2/3 text-center md:text-left"}>
                         I'm a junior at Purdue University working on my Computer Science Degree.
                         This page is made so I can showcase all of my personal projects in tech,
                         as well as just misc projects that I do in other fields.
                     </div>
                 </div>
-                <div className={"md:mr-64 transition-all delay-75 duration-300 hover:scale-90"}>
+                <div className={"pl-28 md:pl-0 w-4/5 lg:w-3/5 md:mr-11 lg:mr-64  transition-all delay-75 duration-300 hover:scale-90"}>
                     <img src={"../src/assets/ProfilePicture.jpg"}  className={"rounded-lg"} alt="Profile" />
                 </div>
             </div>
 
-            <div className={"flex flex-col items-end justify-end pr-64 py-24 "}>
-                <div className={"text-5xl font-bold pb-1/10"}>Interests</div>
-                <div className={"w-1/3 text-right"}>
+
+            <div className={"flex flex-col items-center md:items-end md:justify-end md:pr-20 xl:pr-64 py-24 transition-all delay-75 duration-300"}>
+                <div className={"text-center pb-5 md:text-right text-5xl font-bold pb-1/10 transition-all delay-75 duration-300"}>Interests</div>
+                <div className={"w-1/3 text-center md:text-right transition-all delay-75 duration-300"}>
                     I'm interested in Computer Graphics, Operating Systems, Machine Learning, and Game Development.
                     I'm always open to learning new technologies, and as of December 2023, I'm trying to move more into standalone
                     application development. I'm looking to learn more about OpenGL, OS Dev, and Unity.
